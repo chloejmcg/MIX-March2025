731c01a0eab1985f72957bfb0943a9dc7a224ef6
chloejmcgov <cjm423@cornell.edu>
2025-04-18 22:55:09 -0400

    add RStudio files to .gitignore

---
+++ Code/colors.R
@@ -0,0 +1,6 @@
+treatment_colors <- c(
+  "No Injection" = "lightsteelblue1",
+  "Water" = "lightskyblue2",
+  "Mix Dose 1" = "powderblue",
+  "Mix Dose 2" = "cadetblue3",
+  "Mix Dose 3" = "lightseagreen")
