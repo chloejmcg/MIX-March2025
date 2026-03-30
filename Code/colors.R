bb0d25cc064509ddfc1bb41069961f7cf9c8d8dd
Chloe <cjm423@cornell.edu>
2026-03-30 18:00:17 -0400

    Set up MIX 16S pipeline directory structure

--- Code/colors.R
+++ Code/colors.R
@@ -4,3 +4,8 @@ treatment_colors <- c(
   "Mix Dose 1" = "powderblue",
   "Mix Dose 2" = "cadetblue3",
   "Mix Dose 3" = "lightseagreen")
+mix_colors <- c(
+  "MIX1" = "coral2",
+  "MIX2" = "goldenrod1",
+  "MIX3" = "mediumseagreen"
+)
