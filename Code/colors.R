35456741b3ce19e60ef2417650e7ef39b0d108b7
Chloe <cjm423@cornell.edu>
2026-03-31 09:57:34 -0400

    Quality analysis on 16S files and clearing up git

--- Code/colors.R
+++ Code/colors.R
@@ -1,11 +1,13 @@
 treatment_colors <- c(
-  "No Injection" = "lightsteelblue1",
-  "Water" = "lightskyblue2",
-  "Mix Dose 1" = "powderblue",
-  "Mix Dose 2" = "cadetblue3",
-  "Mix Dose 3" = "lightseagreen")
+  "No Injection" = "#666666",
+  "Water" = "goldenrod1",
+  "Mix Dose 1" = "coral2",
+  "Mix Dose 2" = "cyan3",
+  "Mix Dose 3" = "mediumseagreen")
+
+
 mix_colors <- c(
   "MIX1" = "coral2",
-  "MIX2" = "goldenrod1",
+  "MIX2" = "cyan3",
   "MIX3" = "mediumseagreen"
 )
